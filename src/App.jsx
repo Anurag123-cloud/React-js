e6dc3ce12f57483e8f9655a9a7725a86b5da738a
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-08-20 18:07:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./component/Navigation.jsx";
+import Banneer from "./component/banneer.jsx";
+
+const App=()=>{
+    return <div className="container">
+
+       <Navigation/>
+    <Banneer/>
+
+    </div>
+    ;
+};
+export default App;
---
+++ src/component/Banneer.jsx
@@ -0,0 +1,27 @@
+const  Banneer = () => {
+    return <section className="banner">
+    <div className="column1">
+    <h1>YOUR FEET <br></br>DESERVE<br></br>THE BEST</h1>
+    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+    <div className="button2">
+    <button className="shop" >Shop now</button>
+    <button className="category" >Category</button>
+     </div>
+     <p clas>Also Available On</p>
+     <div className="band">
+    <div className="fac">
+        <img src="/image/flipkart.png" alt="filpkart"/>
+    </div>
+    <div className="amz">
+    <img src="/image/amazon.png" alt="amazon"/>
+    </div>
+
+   </div>
+
+    </div>
+    <div className="column2">
+    <img src="/image/shoe_image.png" alt="show"/>
+    </div>
+   </section>
+}
+export default Banneer;
